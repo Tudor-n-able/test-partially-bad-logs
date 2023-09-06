851c7cafab72524c91e3a97b1d5f5cac9c42acfc
Tudor-n-able <138568618+Tudor-n-able@users.noreply.github.com>
2023-09-06 10:38:22 +0300

    Update Context.java

--- Context.java
+++ Context.java
@@ -1,17 +1,78 @@
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-  <head>
-    <title>503 first byte timeout</title>
-  </head>
-  <body>
-    <h1>Error 503 first byte timeout</h1>
-    <p>first byte timeout</p>
-    <h3>Error 54113</h3>
-    <p>Details: cache-fra-eddf8230041-FRA 1693905953 1746363289</p>
-    <hr>
-    <p>Varnish cache server</p>
-  </body>
-</html>
+import java.util.logging.LogManager;
+
+@Slf4j
+public class Test {
+
+    private static Logger logger = LoggerFactory.getLogger(Slf4j.class);
+    private String orderstatus = "finished";
+    private int errorcode = 404;
+    private String user = "N-able";
+    private Long transactionId = 123L;
+
+    void myFunc0() {
+        logger.info(String.format("Log message info: orderstatus={%s}, errorcode={%d}, user={%s}, transactionid={%d}", orderstatus, errorcode, user, transactionId));
+    }
+
+    void myFunc1() {
+        Logger localLogger = (Logger) LoggerFactory.getLogger(Slf4j.class);
+        localLogger.error(String.format("Log message error: orderstatus={%s}, errorcode={%d}, user={%s}, transactionid={%d}",
+                orderstatus,
+                errorcode,
+                user,
+                transactionId
+        ));
+
+        int a = 10;
+
+        for(int i = 0; i < a; i++) {
+            log.warning(String.format("Log message warn: orderstatus={%s}, errorcode={%d}, user={%s}, transactionid={%d}", orderstatus, errorcode,
+                    user,
+                    transactionId
+            ));
+        }
+
+        LogManager
+                .
+                getLogger().info(String.
+                        format(
+                                "Log message info: orderstatus={%s}, errorcode={%d}, user={%s}, transactionid={%d}",
+                                orderstatus, errorcode,
+                                user, transactionId
+                        )
+                );
+    }
+
+    void myFunc2() {
+        LoggerFactory
+                .getLogger(Slf4j.class)
+                .warning(
+                    String.format("Log message warn: orderstatus={%s}, transactionid={%d}",
+                            orderstatus,
+                            transactionId
+                    ));
+
+
+        Logger.getLogger().info(String.format("Log message warn:  errorcode={%d}, user={%s}",
+                errorcode,
+                user
+        ));
+    }
+
+    void myFunc3() {
+        log.debug("Log message warn: orderstatus=" + orderstatus + ", errorcode=" + errorcode + ", user=" + user + ", transactionid=" + transactionId.toString());
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.myFunc0();
+
+        LogManager.getLogManager().getLogger("LogManager").warning("Log message warn: orderstatus=" +
+                test.orderstatus + ", errorcode=" + test.errorcode +
+                ", user=" + test.user + ", transactionid=" +
+                test.transactionId.toString());
+    }
+}
